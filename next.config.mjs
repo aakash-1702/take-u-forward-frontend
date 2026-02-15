9de8b2907cdb7a8caf3a469a8e854d07f33029d6
Akash <dwivediakash1702@gmail.com>
2026-02-15 09:24:57 +0530

    last changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'https://basecase-backend.onrender.com/api/v1/:path*',
+        source: "/api/v1/:path*", // ← Match /api/v1/...
+        destination: "https://basecase-backend.onrender.com/api/v1/:path*",
       },
     ];
   },
